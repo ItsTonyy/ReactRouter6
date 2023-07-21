557a40578242069e8452b795612505a39ea7ec3f
Tony <tonytnportela@outlook.com>
2023-07-20 22:20:12 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div className='w-screen h-screen bg-zinc-900' >
+      <div className='text-2xl text-white'>
+        <p>hello world!</p>
+      </div> 
+    </div>
+  )
+}
+
+export default App
